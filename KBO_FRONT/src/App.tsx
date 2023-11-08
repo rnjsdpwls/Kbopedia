96b2a440f8f279438a97945431397ea3f1f57959
songtaejin <songtj2743@naver.com>
2023-11-08 11:16:38 +0900

    refactor: app.tsx 정상 파일로 변경

--- KBO_FRONT/src/App.tsx
+++ KBO_FRONT/src/App.tsx
@@ -1,24 +1,49 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import GlobalHeader from './components/Header/GlobalHeader';
 import Community from "./components/pages/CommunityPage/Community";
+import Login from "./components/pages/login/Login";
+import Signup from "./components/pages/signup/SignupId";
 import Posting from "./components/pages/Posting/Posting";
-import Header from "./components/Header/Header";
-import MainPage from "./components/pages/Main/MainPage";
-import Schedule from "./components/pages/Schedule/Schedule";
+import Bears from "./components/pages/TeamBoard/Bears";
+import Heroes from "./components/pages/TeamBoard/Heroes";
+import Dinos from "./components/pages/TeamBoard/Dinos";
+import Landers from "./components/pages/TeamBoard/Landers";
+import Twins from "./components/pages/TeamBoard/Twins";
+import Tigers from "./components/pages/TeamBoard/Tigers";
+import Giants from "./components/pages/TeamBoard/Giants";
+import Lions from "./components/pages/TeamBoard/Lions";
+import Eagles from "./components/pages/TeamBoard/Eagles";
+import Wiz from "./components/pages/TeamBoard/Wiz";
+import MainPage from './components/pages/MainPage/MainPage';
+import TeamInfo from "./components/pages/TeamInfoPage/TeamInfo";
+import TeamItems from "./components/pages/TeamInfoPage/TeamItems";
+import Dict from "./components/pages/DictPage/Dict";
 
 function App() {
-
   return (
-    <BrowserRouter>
-    <Header/>
     <div className="App">
+      <GlobalHeader />
       <Routes>
-        <Route path="/" element={<MainPage/>}></Route>
-        <Route path="/Community" element={<Community/>}></Route>
-        <Route path="/Posting" element={<Posting/>}></Route>
-        <Route path="/Schedule" element={<Schedule/>}></Route>
+        <Route path="/" element={<MainPage />}></Route>
+        <Route path="/Community" element={<Community />}></Route>
+        <Route path="/Login" element={<Login />}></Route>
+        <Route path="/Signup" element={<Signup />}></Route>
+        <Route path="/Posting" element={<Posting />}></Route>
+        <Route path="/Heroes" element={<Heroes />}></Route>
+        <Route path="/Bears" element={<Bears />}></Route>
+        <Route path="/Dinos" element={<Dinos />}></Route>
+        <Route path="/Landers" element={<Landers />}></Route>
+        <Route path="/Twins" element={<Twins />}></Route>
+        <Route path="/Tigers" element={<Tigers />}></Route>
+        <Route path="/Giants" element={<Giants />}></Route>
+        <Route path="/Lions" element={<Lions />}></Route>
+        <Route path="/Eagles" element={<Eagles />}></Route>
+        <Route path="/Wiz" element={<Wiz />}></Route>
+        <Route path="/TeamInfo" element={<TeamInfo newId={1} />} />
+        <Route path="/TeamItems" element={<TeamItems newId={1} />} />
+        <Route path="/Dict" element={<Dict />}></Route>
       </Routes>
-    </div>
-    </BrowserRouter>
+    </div >
   );
 }
 
