6b174dc9dfdad925313a6830c96098ec445f840e
songtaejin <songtj2743@naver.com>
2023-11-20 01:23:58 +0900

    css수정

--- KBO_FRONT/src/components/pages/Schedule/GameResultsTable.tsx
+++ KBO_FRONT/src/components/pages/Schedule/GameResultsTable.tsx
@@ -6,12 +6,13 @@ interface GameResult {
   time: string;
   result: string;
   stadium: string;
+  
 
 }
 function GameResultsTable() {
   const gameResults = gameResultsData.gameResults;
 
-  // 나머지 코드는 이전과 동일합니다
+
 
   return (
     <div className="centered" style={{ position: 'relative', top: '60px', margin: '0 auto', width: '1300px', marginTop: '200px' }}>
@@ -21,18 +22,18 @@ function GameResultsTable() {
           <div style={{ margin: '0 auto', display: 'flex', flexDirection: 'row', justifyContent: 'center', marginRight: "-3px" }}>
             <p style={{ textAlign: 'center', margin: '30px 25px', lineHeight: '2px', verticalAlign: 'middle', flex: 1 }}>날짜</p>
             <p style={{ textAlign: 'center', margin: '30px', lineHeight: '2px', verticalAlign: 'middle', flex: 1 }}>시간</p>
-            <p style={{textAlign: 'center', margin: '30px', lineHeight: '2px', verticalAlign: 'middle', flex: 1 }}>경기</p>
+            <p style={{ textAlign: 'center', margin: '30px', lineHeight: '2px', verticalAlign: 'middle', flex: 1 }}>경기</p>
             <p style={{ textAlign: 'center', margin: '30px', lineHeight: '2px', verticalAlign: 'middle', flex: 1 }}>구장</p>
           </div>
           <hr style={{ width: '1300px', height: '2px', border: '0', backgroundColor: 'green' }} /> {/* 초록색 */}
           <div style={{ margin: '0 auto', display: 'flex', flexDirection: 'row' }}>
             <div key={resultIndex} style={{ flex: 1, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-              <p>{result.date}</p>
-              <p>{result.time}</p>
-              <p>{result.result}</p>
-              <p>{result.stadium}</p>
+              <p style={{ textAlign: 'center', margin: '30px',  verticalAlign: 'middle', flex: 1 }}>{result.date}</p>
+              <p style={{ textAlign: 'center', margin: '30px',  verticalAlign: 'middle', flex: 1 }}>{result.time}</p>
+              <p style={{ textAlign: 'center', margin: '30px',  verticalAlign: 'middle', flex: 1 }}>{result.result}</p>
+              <p style={{ textAlign: 'center', margin: '30px',  verticalAlign: 'middle', flex: 1 }}>{result.stadium}</p>
             </div>
-            <hr style={{ width: '1300px', height: '2px', border: '0', backgroundColor: 'green' }} /> {/* 초록색 */}
+            
             </div>
         </div>
       ))}
