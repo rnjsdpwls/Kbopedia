d2a0c117d9db4a24ce3be87ae6502a396349186b
songtaejin <songtj2743@naver.com>
2023-11-22 01:05:00 +0900

    코드 분리

--- KBO_FRONT/src/components/pages/Schedule/GameResultsTable.tsx
+++ KBO_FRONT/src/components/pages/Schedule/GameResultsTable.tsx
@@ -16,15 +16,16 @@ function GameResultsTable() {
 
   return (
     <div className="centered" style={{ position: 'relative', top: '60px', margin: '0 auto', width: '1300px', marginTop: '200px' }}>
-      {gameResults.map((result: GameResult, resultIndex: number) => (
-        <div key={resultIndex}>
-          <hr style={{ width: '1300px', height: '2px', border: '0', backgroundColor: 'green' }} /> {/* 초록색 */}
+       <hr style={{ width: '1300px', height: '2px', border: '0', backgroundColor: 'green' }} /> {/* 초록색 */}
           <div style={{ margin: '0 auto', display: 'flex', flexDirection: 'row', justifyContent: 'center', marginRight: "-3px" }}>
             <p style={{ textAlign: 'center', margin: '30px 25px', lineHeight: '2px', verticalAlign: 'middle', flex: 1 }}>날짜</p>
             <p style={{ textAlign: 'center', margin: '30px', lineHeight: '2px', verticalAlign: 'middle', flex: 1 }}>시간</p>
             <p style={{ textAlign: 'center', margin: '30px', lineHeight: '2px', verticalAlign: 'middle', flex: 1 }}>경기</p>
             <p style={{ textAlign: 'center', margin: '30px', lineHeight: '2px', verticalAlign: 'middle', flex: 1 }}>구장</p>
           </div>
+      {gameResults.map((result: GameResult, resultIndex: number) => (
+        <div key={resultIndex}>
+          
           <hr style={{ width: '1300px', height: '2px', border: '0', backgroundColor: 'green' }} /> {/* 초록색 */}
           <div style={{ margin: '0 auto', display: 'flex', flexDirection: 'row' }}>
             <div key={resultIndex} style={{ flex: 1, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
