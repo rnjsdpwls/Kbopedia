42147238285d7f273cd3823e4c255c45790bcf86
songtaejin <songtj2743@naver.com>
2023-11-24 03:01:21 +0900

    날짜 시간 경기 구장

--- KBO_FRONT/src/components/pages/Schedule/GameResultsTable.tsx
+++ KBO_FRONT/src/components/pages/Schedule/GameResultsTable.tsx
@@ -14,18 +14,31 @@ interface GameResult {
 function GameResultsTable() {
   const gameResults = gameResultsData.gameResults;
 
+  // Initialize a flag to track whether the header has been displayed
+  let headerDisplayed = false;
+
   return (
     <div className="centered" style={{ position: 'relative', top: '0px', margin: '0 auto', width: '1300px', marginTop: '200px' }}>
       <hr style={{ width: '1300px', height: '2px', border: '0', backgroundColor: 'green' }} />
-      <div style={{ margin: '0 auto', display: 'flex', flexDirection: 'row', justifyContent: 'center', marginRight: "-3px" }}>
-        <p style={{ textAlign: 'center', margin: '30px 25px', lineHeight: '2px', verticalAlign: 'middle', flex: 1 }}>날짜</p>
-        <p style={{ textAlign: 'center', margin: '30px', lineHeight: '2px', verticalAlign: 'middle', flex: 1 }}>시간</p>
-        <p style={{ textAlign: 'center', margin: '30px', lineHeight: '2px', verticalAlign: 'middle', flex: 1 }}>경기</p>
-        <p style={{ textAlign: 'center', margin: '30px', lineHeight: '2px', verticalAlign: 'middle', flex: 1 }}>구장</p>
-      </div>
+
+      {/* Header Row */}
+      {!headerDisplayed && (
+        <div style={{ margin: '0 auto', display: 'flex', flexDirection: 'row' }}>
+          <p style={{ textAlign: 'center', margin: '30px 25px', lineHeight: '2px', verticalAlign: 'middle', flex: 1 }}>날짜</p>
+          <p style={{ textAlign: 'center', margin: '30px', lineHeight: '2px', verticalAlign: 'middle', flex: 1 }}>시간</p>
+          <p style={{ textAlign: 'center', margin: '30px', lineHeight: '2px', verticalAlign: 'middle', flex: 1 }}>경기</p>
+          <p style={{ textAlign: 'center', margin: '30px', lineHeight: '2px', verticalAlign: 'middle', flex: 1 }}>구장</p>
+        </div>
+      )}
+
+      {/* Set the headerDisplayed flag to true after displaying the header */}
+      {!headerDisplayed && (headerDisplayed = true)}
+
+      <hr style={{ width: '1300px', height: '2px', border: '0', backgroundColor: 'green' }} />
+
+      {/* Results */}
       {gameResults.map((result: GameResult, resultIndex: number) => (
         <div key={resultIndex}>
-          <hr style={{ width: '1300px', height: '2px', border: '0', backgroundColor: 'green' }} />
           <div style={{ margin: '0 auto', display: 'flex', flexDirection: 'row' }}>
             <div key={resultIndex} style={{ flex: 1, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
               <p style={{ textAlign: 'center', margin: '30px', verticalAlign: 'middle', flex: 1 }}>{result.date}</p>
