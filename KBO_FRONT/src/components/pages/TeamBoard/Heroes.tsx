423ec15373d282d34085690b3b39a02ee2003fb0
rnjsdpwls59 <rnjsdpwls59@gmail.com>
2023-11-27 16:50:28 +0900

    refactor: 구단별 커뮤니티 수정

--- KBO_FRONT/src/components/pages/TeamBoard/Heroes.tsx
+++ KBO_FRONT/src/components/pages/TeamBoard/Heroes.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Container, ImagePosition, PostBtnPosition, StyeldImages, StyledTable } from '../CommunityPage/StyledCommunity';
+import { Container, ImagePosition, PostBtnPosition, StyeldImages, Pagebutton,StyledTable } from '../CommunityPage/StyledCommunity';
 import axios from 'axios';
 import PostBtn from '../CommunityPage/PostBtn';
 import Heroes from '../../../images/teamLogo/Heroes_logo.png'
@@ -14,9 +14,12 @@ interface DataItem {
 
 
 export default function Community() {
+  const ITEMS_PER_PAGE = 6;
   const [loading, setLoading] = useState<boolean>(true); // 로딩 상태를 나타내는 변수
   const [data, setData] = useState<DataItem[]>([]);
   const [heroesData, setHeroesData] = useState<DataItem[]>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
   useEffect(() => {
     axios.get('http://localhost:3001/all')
       .then((response) => {
@@ -38,8 +41,11 @@ export default function Community() {
   }, []);
 
 
-  const header = ["번호", "제목", "글쓴이", "등록일", "조회"];
+  const totalPages = Math.ceil(data.length / ITEMS_PER_PAGE);
 
+  const header: string[] = ["번호", "제목", "글쓴이", "등록일", "조회"];
+
+  const paginatedData = data.slice((currentPage - 1) * ITEMS_PER_PAGE, currentPage * ITEMS_PER_PAGE);
 
 
   return (
@@ -58,22 +64,41 @@ export default function Community() {
             </tr>
           </thead>
           <tbody>
-            {heroesData.map((item, index) => (
-              <tr key={index}>
-                <td>{item.num}</td>
-                <td>{item.title}</td>
-                <td>{item.name}</td>
-                <td>{item.date}</td>
-                <td>{item.views}</td>
+            {loading ? (
+              <tr>
+                <td colSpan={header.length}>로딩 중...</td>
               </tr>
-            ))}
+            ) : (
+              paginatedData.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.num}</td>
+                  <td>{item.title}</td>
+                  <td>{item.name}</td>
+                  <td>{item.date}</td>
+                  <td>{item.views}</td>
+                </tr>
+              ))
+            )}
           </tbody>
         </StyledTable>
+
+        <div>
+          {/* Display page numbers */}
+          {Array.from({ length: totalPages }).map((_, index) => (
+            <Pagebutton
+              key={index}
+              onClick={() => setCurrentPage(index + 1)}
+              style={{ fontWeight: currentPage === index + 1 ? 'bold' : 'normal' }}
+            >
+              {index + 1}
+            </Pagebutton>
+          ))}
+        </div>
       </Container>
+
       <PostBtnPosition>
         <PostBtn label={"글쓰기"}></PostBtn>
       </PostBtnPosition>
     </div>
   );
 }
-//////////////////...
