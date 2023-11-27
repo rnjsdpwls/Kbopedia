d9e47898811bfa0c1d0b85164ad37e3ec37c9929
kanghyun <kmsyee@gmail.com>
2023-11-27 19:04:55 +0900

    포스팅 페이지 수정

--- KBO_FRONT/src/components/pages/Posting/Posting.tsx
+++ KBO_FRONT/src/components/pages/Posting/Posting.tsx
@@ -1,36 +1,112 @@
-import React, { useEffect, useRef, useState } from "react";
-import { StyledContent, StyledList, StyledTitle } from "./StyledPosting";
-import { CreateTitle } from "./CreateTitle";
-import CreateContent from "./CreateContent";
-import SaveBtn from "./SaveBtn";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Container, StyledContent, StyledTable, PostBtnPosition, StyledPostBtn } from './StyledPosting';
+import { useNavigate } from 'react-router-dom';
 
-interface Props {
-    "제목": string
-    "내용": string
+interface Post {
+    id: number;
+    title: string;
+    content: string;
+    created_at: string;
+    updated_at: string;
+    count: number;
 }
 
-export default function Posting() {
+interface NewPost {
+    title: string;
+    content: string;
+}
 
-    const [title, setTitle] = useState<string>('');
-    const [isLoading, setIsLoading] = useState(false);
-    const headers = ["제목", "내용"];
-    // const listItem = headers.map((header) => {
-    //     <li>{header}</li>
-    //     return (
-    //         <ul>
-    //             <CreateTitle />
-    //         </ul>
-    //     )
-    // })
+const BulletinBoard: React.FC = () => {
+    const [posts, setPosts] = useState<Post[]>([]);
+    const [newPost, setNewPost] = useState<NewPost>({ title: '', content: '' });
+    const [editPost, setEditPost] = useState<Post | null>(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get<Post[]>('http://127.0.0.1:8000/api/');
+                setPosts(response.data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    const handleInputChange = (
+        e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+        isEditing: boolean = false
+    ) => {
+        const targetPost = isEditing ? editPost! : newPost;
+        const updatedPost = {
+            ...targetPost,
+            [e.target.name]: e.target.value,
+        };
+
+        isEditing ? setEditPost(updatedPost as Post) : setNewPost(updatedPost as NewPost);
+    };
+    const formatDate = (dateString: string): string => {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' } as any;
+
+        return new Date(dateString).toLocaleDateString('ko-KR', options);
+    };
+
+
+    const handlePostSubmit = async () => {
+        try {
+            const response = await axios.post<Post>('http://127.0.0.1:8000/api/', newPost);
+            setPosts([...posts, response.data]);
+            setNewPost({ title: '', content: '' });
+
+        } catch (error) {
+            console.log(error);
+        }
+        navigate('/community');
+
+    };
 
 
     return (
-        <div>
-            {/* <StyledList>
-                <li>{headers}</li>
-            </StyledList> */}
-            <CreateTitle />
-            <SaveBtn label="등록하기"></SaveBtn>
-        </div>
-    )
-}
+        <Container>
+            <StyledTable>
+            <h1>글 작성</h1>
+                <tr>
+                    <td>제목</td>
+                    <td colSpan={4}><input
+                        type="text"
+                        name="title"
+                        value={newPost.title}
+                        onChange={(e) => handleInputChange(e)}
+                    />
+                    </td>
+                </tr>
+                <tr>
+                    <td>글쓴이</td>
+                    <td>닉네임 부분</td>
+                    <td>응원구단</td>
+                    <td>응원구단명</td>
+                </tr>
+                <tr>
+                    <td>내용</td>
+                    <td colSpan={4}>
+                        <StyledContent>
+                            <textarea
+                                name="content"
+                                value={newPost.content}
+                                onChange={(e) => handleInputChange(e)}
+                            />
+                        </StyledContent>
+                    </td>
+                </tr>
+            </StyledTable>
+                <PostBtnPosition>
+                    <StyledPostBtn onClick={handlePostSubmit}>글쓰기</StyledPostBtn>
+                </PostBtnPosition>
+        </Container>
+    );
+};
+
+export default BulletinBoard;
--- KBO_FRONT/src/components/pages/Posting/StyledPosting.ts
+++ KBO_FRONT/src/components/pages/Posting/StyledPosting.ts
@@ -1,60 +1,79 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
-export const StyledTitle = styled.div`
-    text-align:center;
-    width : 30%;
-    border: 4px solid #687769;
-    margin : 0 auto;
-    margin-top:50px;
-    
-    
-    input{
-        font-family: 'KBO-Dia-Gothic_bold';
-        border:none;
-        outline:none;
-        background-color:transparent;
-        &::placeholder{
-            color:#3b649461;
-        }
-    }
+export const Container = styled.div`
+
 `
+// display:flex;
+export const StyledTable = styled.table`
+    margin-top : 250px;
+    width : 75%;
+    height : 100px;
+    text-align:center;
+    margin : 0 auto;
+    tr {
+        border-top: 3px solid #5c9b57;
+        line-height:60px;
+    }
+    td {
+        border: 3px solid #5C9B57;
+        width : 25%;
+    }
+`;
+// border-collapse: collapse;
+// font-family: 'KBO-Dia-Gothic_medium';
+
+// width: 1000px;
+// margin-top: 150px;
+// margin-right:auto;
+// @media (min-width: 1000px) {
+// }
+// tr {
+//     border-top: 3px solid #5C9B57;
+//     text-align: center;
+//     font-size:28px;
+//     line-height:60px;
+// }
+// td{
+//     font-family: 'KBO-Dia-Gothic_light';
+//     font-size:16px;
+// } 
 
 
 export const StyledContent = styled.div`
-    textarea {
-        display: flex;
-        width: 30%;
-        margin: 0 auto;
-        margin-top: 50px;
-        height: 250px;
-        overflow: auto; /* 내용이 넘칠 때 스크롤 표시 */
-    
-        justify-content: space-between;
-        border: 4px solid #687769;
-        font-family: 'KBO-Dia-Gothic_medium';
-        background-color: transparent;
-        resize: none;
-        &::placeholder{
-            color:#3b649461;
-        }
-    }
 `;
+// textarea {
+//     display: flex;
+//     width: 30%;
+//     margin: 0 auto;
+//     margin-top: 50px;
+//     height: 250px;
+//     overflow: auto; /* 내용이 넘칠 때 스크롤 표시 */
 
-export const StyledList = styled.ul`
-    list-style:none;
-`
+//     justify-content: space-between;
+//     border: 4px solid #687769;
+//     font-family: 'KBO-Dia-Gothic_medium';
+//     background-color: transparent;
+//     resize: none;
+//     &::placeholder{
+//         color:#3b649461;
+//     }
+// }
 
-export const StyledButton = styled.button`
+
+export const StyledPostBtn = styled.button`
     border-radius:5px;
     border-color:transparent;
     background-color:#5C9B57;
-    position:fixed;
-    bottom:50px;
-    right:100px;
+    color:#ffffff;
     width:100px;
     height:50px;
-    color:#ffffff;
-
     font-family:'KBO-Dia-Gothic_light';
+    font-size:16px;
 `
 
+export const PostBtnPosition = styled.div`
+    width:1100px;
+    height:50px;
+    margin:120px auto;
+    text-align:right;
+`   
