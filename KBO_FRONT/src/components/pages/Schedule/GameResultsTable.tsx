2b5b3de68d1238ebd4bacb564ace47c8e3973894
songtaejin <songtj2743@naver.com>
2023-12-09 16:54:49 +0900

    코드 수정

--- KBO_FRONT/src/components/pages/Schedule/GameResultsTable.tsx
+++ KBO_FRONT/src/components/pages/Schedule/GameResultsTable.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import gameResultsData from '../../../db/gameResults.json';
-import { CellA, CellB, CellC, CellE} from './GameResultsTableA';
+import { TimeA, Same, Time, Above, TeamandScore, CellD, Hline, HrB} from './GameResultsTableA';
 interface GameResult {
   date: string;
   time: string;
@@ -19,22 +19,22 @@ function GameResultsTable() {
 
   return (
     <div className="centered" style={{ position: 'relative', top: '0px', margin: '0 auto', width: '1300px', marginTop: '200px' }}>
-      <hr style={{ width: '1300px', height: '2px', border: '0', backgroundColor: 'green' }} />
+      <Hline></Hline>
 
       {/* Header Row */}
       {!headerDisplayed && (
-        <div style={{ margin: '0 auto', display: 'flex', flexDirection: 'row' }}>
-          <CellA>날짜</CellA>
-          <CellB>시간</CellB>
-          <CellB>경기</CellB>
-          <CellB>구장</CellB>
-        </div>
+        <Above>
+          <TimeA>날짜</TimeA>
+          <Same>시간</Same>
+          <Same>경기</Same>
+          <Same>구장</Same>
+        </Above>
       )}
 
       {/* Set the headerDisplayed flag to true after displaying the header */}
       {!headerDisplayed && (headerDisplayed = true)}
 
-      <hr style={{ width: '1300px', height: '2px', border: '0', backgroundColor: 'green' }} />
+      <Hline></Hline> 
 
       {/* Results */}
       {gameResults.map((result: GameResult, resultIndex: number) => (
@@ -44,23 +44,23 @@ function GameResultsTable() {
               <p>{result.date}</p>
             </div>
         <div style={{ flex: 1, display: 'flex', flexDirection: 'column'}}>
-          <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
-          <CellC>{result.time}</CellC>
-          <CellE>{`${result.team1} ${result.score1} vs ${result.team2} ${result.score2}`}</CellE>
-          <CellE>{result.stadium}</CellE>
-          </div> 
+          <CellD>
+            <Time>{result.time}</Time>
+            <TeamandScore>{`${result.team1} ${result.score1} vs ${result.team2} ${result.score2}`}</TeamandScore>
+           <TeamandScore>{result.stadium}</TeamandScore>
+          </CellD> 
 
-          <hr style={{ width: '1000px', height: '2px', border: '0', backgroundColor: 'green' }} /> {/* 초록색 */}
+          <HrB></HrB>   {/* 초록색 */}
 
-          <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
-          <CellC>{result.time}</CellC>
-          <CellE>{`${result.team1} ${result.score1} vs ${result.team2} ${result.score2}`}</CellE>
-          <CellE>{result.stadium}</CellE>
-           </div>
+          <CellD>
+            <Time>{result.time}</Time>
+            <TeamandScore>{`${result.team1} ${result.score1} vs ${result.team2} ${result.score2}`}</TeamandScore>
+            <TeamandScore>{result.stadium}</TeamandScore>
+          </CellD>
           </div>
         </div>
 
-        <hr style={{ width: '1300px', height: '2px', border: '0', backgroundColor: 'green' }} /> {/* 초록색 */}
+    <Hline></Hline>
 
      
 
