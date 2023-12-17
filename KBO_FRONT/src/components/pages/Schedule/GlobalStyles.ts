d6694684720a5c8e6c6f3bfd9693b8528e96a1f1
songtaejin <songtj2743@naver.com>
2023-12-17 23:00:02 +0900

    폰트 적용

---
+++ KBO_FRONT/src/components/pages/Schedule/GlobalStyles.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'KBO-Dia-Gothic_bold';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2304-2@1.0/KBO-Dia-Gothic_bold.woff') format('woff');
+    font-weight: 700;
+    font-style: normal;
+  }
+`;
+
+export default GlobalStyles;
