6f6716c66d548c3339c1a9b48180049b82808a2f
rnjsdpwls59 <rnjsdpwls59@gmail.com>
2023-12-19 22:27:18 +0900

    refactor: Quiz 파일명 수정

--- KBO_FRONT/src/components/pages/Quiz/QuizList.tsx
+++ KBO_FRONT/src/components/pages/Quiz/QuizList.tsx
@@ -1,45 +1,54 @@
-import React, { useState } from "react";
-import { QuizContainer, QuizMainTitle, QuizLevelContainer, QuizLevelBut, Intro } from './StyleQuizList';
+import React, { useState, useEffect } from 'react';
+import { QuizListContainer, QuizTitle, QuizLeveltitle } from './StyleQuizList';
 import QuizDummy from '../../../db/quizData.json';
 import { Link } from 'react-router-dom';
 
 type QuizData = {
   id: number;
   question: string;
-  option: string[];
+  options: string[];
   correctAnswer: string;
+  completed?: boolean;
 };
 
-type QuizLevelType = 'quizbasic' | 'quizdeep';
+type QuizPageProps = {
+  level: 'quizbasic' | 'quizdeep';
+};
 
-export default function QuizList() {
-  const [quizLevel, setQuizLevel] = useState<QuizLevelType>('quizbasic');
+export default function QuizList({ level }: QuizPageProps) {
+  const [completedQuizzes, setCompletedQuizzes] = useState<number[]>([]);
 
-  const handleLevelChange = (level: QuizLevelType) => {
-    setQuizLevel(level);
-  };
+  useEffect(() => {
+    // 컴포넌트가 처음 렌더링될 때 완료된 퀴즈 목록을 확인하고 상태를 설정
+    const savedCompletedQuizzes = localStorage.getItem('completedQuizzes');
+    if (savedCompletedQuizzes) {
+      setCompletedQuizzes(JSON.parse(savedCompletedQuizzes));
+    }
+  }, []); // 빈 배열을 전달하여 컴포넌트가 처음 렌더링될 때 한 번만 실행
 
-  const filteredQuizzes = QuizDummy[quizLevel];
+ 
+
+  // 퀴즈 목록을 가져올 때 완료 상태를 고려하여 퀴즈 목록을 가져옴
+  const quizzes = QuizDummy[level].map((quiz: QuizData) => ({
+    ...quiz,
+    completed: completedQuizzes.includes(quiz.id),
+  }));
+
+  const levelText = level === 'quizbasic' ? 'Level 1' : 'Level 999';
 
   return (
-    <div>
-      <QuizContainer>
-        <QuizMainTitle>Quiz</QuizMainTitle>
-
-        <Intro>퀴즈의 난이도를 선택해 주세요</Intro>
-
-        <QuizLevelContainer>
-          <Link to="/quiz/basic">
-            <QuizLevelBut onClick={() => handleLevelChange('quizbasic')}>Level 1</QuizLevelBut>
+    <QuizListContainer>
+      <QuizLeveltitle style={{ fontSize: '40px' }}>{levelText} 문제 </QuizLeveltitle>
+      {quizzes.map((quiz: QuizData) => (
+        <div key={quiz.id}>
+          <Link to={`/Quiz/${quiz.id}`}>
+            <QuizTitle>
+              {quiz.question}
+              {quiz.completed && <span style={{ marginLeft: '10px', color: 'green' }}>완료</span>}
+            </QuizTitle>
           </Link>
-           
-          <Link to="/quiz/deep">
-            <QuizLevelBut onClick={() => handleLevelChange('quizdeep')}>Level 999</QuizLevelBut>
-          </Link>
-        </QuizLevelContainer>
-
-      </QuizContainer>
-    </div>
+        </div>
+      ))}
+    </QuizListContainer>
   );
-
-};
+}
--- KBO_FRONT/src/components/pages/Quiz/QuizPage.tsx
+++ KBO_FRONT/src/components/pages/Quiz/QuizPage.tsx
@@ -1,54 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import { QuizListContainer, QuizTitle, QuizLeveltitle } from './StyleQuizList';
+import React, { useState } from "react";
+import { QuizContainer, QuizMainTitle, QuizLevelContainer, QuizLevelBut, Intro } from './StyleQuizList';
 import QuizDummy from '../../../db/quizData.json';
 import { Link } from 'react-router-dom';
 
 type QuizData = {
   id: number;
   question: string;
-  options: string[];
+  option: string[];
   correctAnswer: string;
-  completed?: boolean;
 };
 
-type QuizPageProps = {
-  level: 'quizbasic' | 'quizdeep';
-};
+type QuizLevelType = 'quizbasic' | 'quizdeep';
 
-export default function QuizPage({ level }: QuizPageProps) {
-  const [completedQuizzes, setCompletedQuizzes] = useState<number[]>([]);
+export default function QuizPage() {
+  const [quizLevel, setQuizLevel] = useState<QuizLevelType>('quizbasic');
 
-  useEffect(() => {
-    // 컴포넌트가 처음 렌더링될 때 완료된 퀴즈 목록을 확인하고 상태를 설정
-    const savedCompletedQuizzes = localStorage.getItem('completedQuizzes');
-    if (savedCompletedQuizzes) {
-      setCompletedQuizzes(JSON.parse(savedCompletedQuizzes));
-    }
-  }, []); // 빈 배열을 전달하여 컴포넌트가 처음 렌더링될 때 한 번만 실행
+  const handleLevelChange = (level: QuizLevelType) => {
+    setQuizLevel(level);
+  };
 
- 
-
-  // 퀴즈 목록을 가져올 때 완료 상태를 고려하여 퀴즈 목록을 가져옴
-  const quizzes = QuizDummy[level].map((quiz: QuizData) => ({
-    ...quiz,
-    completed: completedQuizzes.includes(quiz.id),
-  }));
-
-  const levelText = level === 'quizbasic' ? 'Level 1' : 'Level 999';
+  const filteredQuizzes = QuizDummy[quizLevel];
 
   return (
-    <QuizListContainer>
-      <QuizLeveltitle style={{ fontSize: '40px' }}>{levelText} 문제 </QuizLeveltitle>
-      {quizzes.map((quiz: QuizData) => (
-        <div key={quiz.id}>
-          <Link to={`/Quiz/${quiz.id}`}>
-            <QuizTitle>
-              {quiz.question}
-              {quiz.completed && <span style={{ marginLeft: '10px', color: 'green' }}>완료</span>}
-            </QuizTitle>
+    <div>
+      <QuizContainer>
+        <QuizMainTitle>Quiz</QuizMainTitle>
+
+        <Intro>퀴즈의 난이도를 선택해 주세요</Intro>
+
+        <QuizLevelContainer>
+          <Link to="/quiz/basic">
+            <QuizLevelBut onClick={() => handleLevelChange('quizbasic')}>Level 1</QuizLevelBut>
           </Link>
-        </div>
-      ))}
-    </QuizListContainer>
+           
+          <Link to="/quiz/deep">
+            <QuizLevelBut onClick={() => handleLevelChange('quizdeep')}>Level 999</QuizLevelBut>
+          </Link>
+        </QuizLevelContainer>
+
+      </QuizContainer>
+    </div>
   );
-}
+
+};
